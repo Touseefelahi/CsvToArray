68a38455271fc78c1ac5f078d9f0eb58b361f803
Touseef <Touseef@stira.sa>
2020-07-22 07:30:20 +0300

    net core project added

--- CsvToArrayTest/MainWindow.xaml.cs
+++ CsvToArrayTest/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CsvToArray
+namespace CsvToArrayTest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
